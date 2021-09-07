02bbbdbcde22c11723df02b6297cd385654752fb
taitai1122 <taikicody592@icloud.com>
2021-09-07 20:44:04 +0000

    Initial commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  
   def index
   end
 
@@ -6,8 +7,24 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    
+    book.save
+    
+    redirect_to books_path
   end
 
   def edit
   end
+  
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
-  get 'homes/index'
-  get 'homes/show'
-  get 'homes/new'
-  get 'homes/edit'
+  resources :books
+  root to: 'books#index'
+  
+  resources :homes
+  root to: 'homes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
