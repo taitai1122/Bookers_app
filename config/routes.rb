11a8c39a292fed441d6a9381ba50e95acf82ee6e
taitai1122 <taikicody592@icloud.com>
2021-09-07 14:45:36 +0000

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/edit'
+  get 'homes/index'
+  get 'homes/show'
+  get 'homes/new'
+  get 'homes/edit'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
