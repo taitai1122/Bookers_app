b4092f14b9bafb628ae7462d5c698e88ac322500
taitai1122 <taikicody592@icloud.com>
2021-09-09 13:15:47 +0000

    Initial commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,16 +5,23 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @books = Book.find(params[:id])
   end
 
   def new
     @books = Book.all
+    @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      @books = Book.all
+      render :new
+    end
   end
 
 
@@ -22,6 +29,23 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+    redirect_to new_book_path
+    end
+  end
 
 
   private
